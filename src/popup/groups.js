cfb37c85ce1db93ff4cb11abd1ae10cce5609142
Grant Moyer <grantmoyer@gmail.com>
2018-03-13 23:11:11 -0400

    Stops using my limited popup html

--- src/popup/groups.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(
-    React.createElement('p', {style: {fontFamily: "sans-serif"}}, "Hello, World!"),
-    document.getElementById("content")
-  );
-});
